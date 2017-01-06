fc05e15ca59e9f666a341c31ded06bf431efe50d
gwaygenomics <gregory.way@gmail.com>
2017-01-06 14:14:41 -0500

    add repo initialization script

---
+++ initialize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download necessary index and curation files
+
+url=https://zenodo.org/record/232520/files/tad_pathways_data.tar.gz
+wget $url
+
+# Extract data
+tar -zxvf tad_pathways_data.tar.gz
+
+# Stabilize computing environments in Python and R
+conda env create --quiet --force --file environment.yml
+Rscript scripts/install.R
