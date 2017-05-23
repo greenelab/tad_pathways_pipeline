747b27a4d74db153c5f31b50bf235b3e962b3dd7
gwaygenomics <gregory.way@gmail.com>
2017-05-23 15:54:32 -0400

    modify bmd pipeline example

--- example_pipeline_bmd.sh
+++ example_pipeline_bmd.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+# Example of a TAD_Pathways Analysis applied to Bone Mineral Density GWAS
 set -o errexit
 
-# Example of a TAD_Pathways Analysis applied to Bone Mineral Density GWAS
+gwas_file='data/gwas_catalog/Bone_mineral_density_hg19.tsv'
+snp_file='data/gwas_tad_snps/Bone_mineral_density_hg19_SNPs.tsv'
+tad_file='data/gwas_tad_genes/Bone_mineral_density_hg19_SNPs_TAD_genelists.tsv'
+trait='BMD'
 
-# After saving WebGestalt tsv file, parse its contents
-python scripts/parse_gestalt.py --trait 'bmd'
+# Perform WebGestalt pathway analysis and parse results
+Rscript --vanilla scripts/webgestalt_run.R \
+        --tad_genelist_file $tad_file \
+        --output_name $trait
 
 # Construct an evidence file - Nearest gene to gwas or not
 python scripts/construct_evidence.py \
-        --trait 'bmd'\
-        --gwas 'data/gwas_catalog/Bone_mineral_density_hg19.tsv'\
+        --trait $trait \
+        --gwas $gwas_file \
         --pathway 'skeletal system development'
 
 # Summarize the evidence file
 python scripts/summarize_evidence.py \
-        --evidence 'results/bmd_gene_evidence.csv' \
-        --snps 'data/gwas_tad_snps/Bone_mineral_density_hg19_SNPs.tsv' \
-        --output_file 'results/bmd_gene_evidence_summary.tsv'
+        --evidence 'results/BMD_gene_evidence.csv' \
+        --snps $snp_file \
+        --output_file 'results/BMD_gene_evidence_summary.tsv'
 
 # Visualize overlap in TAD pathways curation
-R --no-save --args 'results/bmd_gene_evidence.csv' \
+R --no-save --args 'results/BMD_gene_evidence.csv' \
         < scripts/integrative_summary.R 
