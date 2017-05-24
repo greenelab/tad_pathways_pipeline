0ec3f806abf85622355baace5fa4a6bfe5f03af4
gwaygenomics <gregory.way@gmail.com>
2017-05-24 10:28:19 -0400

    add T2D example pipeline

--- example_pipeline_t2d.sh
+++ example_pipeline_t2d.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+# Example of a TAD_Pathways Analysis applied to Type 2 Diabetes GWAS
 set -o errexit
 
-# Example of a TAD_Pathways Analysis applied to Type 2 Diabetes GWAS
+gwas_file='data/gwas_catalog/Type_2_diabetes_hg19.tsv'
+snp_file='data/gwas_tad_snps/Type_2_diabetes_hg19_SNPs.tsv'
+tad_file='data/gwas_tad_genes/Type_2_diabetes_hg19_SNPs_TAD_genelists.tsv'
+trait='t2d'
+evidence_file='results/t2d_gene_evidence.csv'
+pathway_p_values_file='gestalt/t2d_pvals.tsv'
 
-# After saving WebGestalt tsv file, parse its contents
-python scripts/parse_gestalt.py --trait 't2d'
+# Perform WebGestalt pathway analysis and parse results
+Rscript --vanilla scripts/webgestalt_run.R \
+        --tad_genelist_file $tad_file \
+        --output_name $trait
 
-# Construct an evidence file - Nearest gene to gwas or not
+# Construct an evidence file
+# In this case, select all significant pathways, no preselection required
 python scripts/construct_evidence.py \
-        --trait 't2d'\
-        --gwas 'data/gwas_catalog/Type_2_diabetes_hg19.tsv'\
-        --pathway 'peptide hormone secretion'
+        --trait $trait \
+        --gwas $gwas_file \
+        --pathway $pathway_p_values_file
 
 # Summarize the evidence file
 python scripts/summarize_evidence.py \
-        --evidence 'results/t2d_gene_evidence.csv' \
-        --snps 'data/gwas_tad_snps/Type_2_diabetes_hg19_SNPs.tsv' \
+        --evidence $evidence_file \
+        --snps $snp_file \
         --output_file 'results/t2d_gene_evidence_summary.tsv'
 
 # Visualize overlap in TAD pathways curation
-R --no-save --args 'results/t2d_gene_evidence.csv' \
+R --no-save --args $evidence_file \
         < scripts/integrative_summary.R 
