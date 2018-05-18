a904c8abeb1ff7d78668462fe799ca6bf3ebafb0
Greg Way <gregory.way@gmail.com>
2018-05-18 09:58:12 -0400

    Add Functionality: Remove HLA TAD and Improve Evidence Summary (#18)
    * add logic to remove tad 1198 (hla)
    
    * add custom saving locations for evidence output
    
    * add try except to convert chromosome
    
    * add custom save location for webgestalt
    
    * add all pathway option to constructing tad evidence
    
    * add more features to summarizing evidence
    
    * comma sep
    
    * remove multiple assign calls

--- scripts/construct_evidence.py
+++ scripts/construct_evidence.py
@@ -22,6 +22,13 @@ Command line: python scripts/construct_evidence.py
                     --pathway 'skeletal system development'
                     --pathway 'skeletal system development,ossification'
 
+    And the following optional flags:
+
+      --results_directory       the directory of where to save results
+      --gestalt_directory       the directory to load gestalt results from
+      --all_pathways            will subset to all significant pathways
+                                and not just consider the top pathway
+
 Output:
 a .csv evidence file (two columns: [gene, type of evidence])
 """
@@ -37,29 +44,46 @@ parser.add_argument("-g", "--gwas", help="location of gwas genelist")
 parser.add_argument("-r", "--group", help="Group to subset evidence file",
                     default=None)
 parser.add_argument("-p", "--pathway", help="pathway of interest")
+parser.add_argument("-d", "--results_directory", help="where to save results",
+                    default="results")
+parser.add_argument("-e", "--gestalt_directory",
+                    help="location of gestalt results", default="gestalt")
+parser.add_argument("-a", "--all_pathways", action='store_true',
+                    help="consider all pathways in evidence")
 args = parser.parse_args()
 
 # Load Constants
 trait = args.trait
 gwas_file = args.gwas
 gwas_group = args.group
+all_pathways = args.all_pathways
 try:
     pathway_df = pd.read_table(args.pathway)
-    pathway = pathway_df[pathway_df['adjP'] < 0.05]['go_name'].tolist()
+    pathway = pathway_df.query('adjP < 0.05').loc[:, 'go_name'].tolist()
 except:
     pathway = args.pathway.split(',')
 
-trait_file = os.path.join('gestalt', '{}_gestalt.tsv'.format(trait))
-output_file = os.path.join('results', '{}_gene_evidence.csv'.format(trait))
+results_dir = args.results_directory
+gestalt_dir = args.gestalt_directory
+
+trait_file = os.path.join(gestalt_dir, '{}_gestalt.tsv'.format(trait))
+if all_pathways:
+    trait = '{}_all-sig-pathways'.format(trait)
+output_file = os.path.join(results_dir, '{}_gene_evidence.csv'.format(trait))
 
 # Load Data
-pathway_genes = pd.read_csv(trait_file, delimiter='\t')
-pathway_genes = pathway_genes.loc[pathway_genes['go_name'].isin(pathway), :]
-pathway_genes = pathway_genes['symbol'].tolist()
+drop_col = ['link', 'count', 'observed', 'expected', 'R', 'overlapGene']
+pathway_genes_df = (
+    pd.read_csv(trait_file, delimiter='\t')
+    .query('go_name in @pathway')
+    .drop(drop_col, axis='columns')
+    )
+
+pathway_genes = pathway_genes_df.loc[:, 'symbol'].tolist()
 
 gwas_genes_df = pd.read_table(gwas_file)
 if gwas_group:
-    gwas_genes_df = gwas_genes_df[gwas_genes_df['group'] == gwas_group]
+    gwas_genes_df = gwas_genes_df.query('group == @gwas_group')
 gwas_genes = gwas_genes_df.MAPPED_GENE
 
 # Process GWAS genes
@@ -70,9 +94,9 @@ for gene in gwas_genes:
 
 # Process all genes
 all_genes = set(full_gwas_genes + pathway_genes)
-all_assignments = []
 
 # Logic to determine genic evidence
+all_assignments = []
 for gene in all_genes:
     if gene in full_gwas_genes:
         assignment = 'gwas'
@@ -82,6 +106,25 @@ for gene in all_genes:
         assignment = 'tad'
     all_assignments.append(assignment)
 
-evidence = pd.DataFrame([all_genes, all_assignments]).T
-evidence.columns = ['gene', 'evidence']
-evidence.to_csv(output_file, sep=',', index=False)
+evidence_df = pd.DataFrame([all_genes, all_assignments]).T
+evidence_df.columns = ['gene', 'evidence']
+
+# Write output to file
+evidence_df = evidence_df.merge(pathway_genes_df, how='left', left_on='gene',
+                                right_on='symbol')
+
+if not all_pathways:
+    evidence_df = (
+        evidence_df
+        .sort_values(by='pval')
+        .groupby('gene', as_index=False)
+        .first()
+        )
+
+(
+    evidence_df
+    .merge(gwas_genes_df, how='left', left_on='gene', right_on='MAPPED_GENE')
+    .set_index('gene')
+    .drop(['Unnamed: 0', 'MAPPED_GENE', 'symbol'], axis='columns')
+    .to_csv(output_file, sep=',', index=True)
+)
