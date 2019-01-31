cc0eb698c4dadda7d9c8a17f893642b80988f048
Greg Way <gregory.way@gmail.com>
2019-01-31 06:54:40 -0500

    [DEVELOP] Update Custom Example Pipeline (#33)
    * remove trait renaming
    
    * reformat biomart call
    
    also adding a single patch (refs #32)
    
    * update example pipeline
    
    * all custom results saved in one folder
    
    * add folder structure

--- example_pipeline_custom.sh
+++ example_pipeline_custom.sh
@@ -5,14 +5,22 @@
 # Prostate Cancer. The data is used as a custom input.
 set -o errexit
 
-# Define filenames
-candidate_snp_file='custom_example.csv'
-candidate_snp_location_file='results/custom_example_location.tsv'
-candidate_snp_tad_file='results/custom_example_tad_results.tsv'
-nearest_gene_file='results/custom_example_tad_results_nearest_gene.tsv'
+# Set constants
 trait='custom'
-evidence_file='results/custom_gene_evidence.csv'
-pathway_p_values_file='gestalt/custom_gestalt.tsv'
+custom_dir='results/custom_example/'
+geneset='geneontology_Biological_Process'
+pathway_name='skeletal_system_development'
+
+# Set input file names
+candidate_snp_file='custom_example.csv'
+
+# Set output file names
+candidate_snp_location_file=$custom_dir'/custom_example_location.tsv'
+candidate_snp_tad_file=$custom_dir'/custom_example_tad_results.tsv'
+nearest_gene_file=$custom_dir'/custom_example_tad_results_nearest_gene.tsv'
+pathway_file=$custom_dir'/'$trait'_gestalt.tsv'
+evidence_file=$custom_dir'/'$trait'_gene_evidence.csv'
+summary_file=$custom_dir'/'$trait'_gene_evidence_summary.tsv'
 
 # Map SNPs to genomic location
 Rscript --vanilla scripts/build_snp_list.R \
@@ -27,21 +35,25 @@ python scripts/build_custom_tad_genelist.py \
 # Perform WebGestalt pathway analysis and parse results
 Rscript --vanilla scripts/webgestalt_run.R \
         --tad_genelist_file $candidate_snp_tad_file \
-        --output_name $trait
+        --output_name $trait \
+        --output_directory $custom_dir
 
 # Construct an evidence file - Nearest gene to gwas or not
 python scripts/construct_evidence.py \
-            --trait $trait \
-            --gwas_file $nearest_gene_file \
-            --pathway_file $pathway_p_values_file \
-            --all_sig_pathways
+        --trait $trait \
+        --gwas_file $nearest_gene_file \
+        --pathway_file $pathway_file \
+        --pathway "top" \
+        --output_directory $custom_dir
 
 # Summarize the evidence file
 python scripts/summarize_evidence.py \
-            --evidence $evidence_file \
-            --snps $candidate_snp_tad_file \
-            --output_file 'results/custom_gene_evidence_summary.tsv'
+        --evidence $evidence_file \
+        --snps $candidate_snp_tad_file \
+        --output_file $summary_file
 
 # Visualize overlap in TAD pathways curation
-R --no-save --args $evidence_file $trait \
-                < scripts/integrative_summary.R
+Rscript --vanilla scripts/integrative_summary.R \
+        --evidence_file $evidence_file \
+        --trait $trait \
+        --output_directory $custom_dir
